2c3edb753b18ed1951233fb63a292bb941999ec3
Lucas Marques da Silva <111651260+LucasMKS@users.noreply.github.com>
2025-06-21 11:36:51 -0300

    Update page.tsx

--- src/app/grupos/[id]/page.tsx
+++ src/app/grupos/[id]/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useEffect, useState, useMemo } from "react";
-import { useRouter } from "next/navigation";
 import { motion, AnimatePresence } from "framer-motion";
 import {
   listarTarefasPorGrupo,
